640e8f4f644adbfa82c871d0d6db2694ab1c6275
Eduardo Macedo <eduzemacedo@hotmail.com>
2023-04-09 00:52:57 -0300

    Implements form data for user registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
-import { TuiRootModule, TuiDialogModule, TuiAlertModule, TuiButtonModule } from '@taiga-ui/core';
-import { TuiInputModule, TuiIslandModule, TuiInputPasswordModule, TuiInputPhoneModule } from '@taiga-ui/kit';
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TuiButtonModule, TuiErrorModule } from '@taiga-ui/core';
+import {
+  TuiInputModule,
+  TuiIslandModule,
+  TuiInputPasswordModule,
+  TuiInputPhoneModule,
+  TuiFieldErrorPipeModule,
+} from '@taiga-ui/kit';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -8,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, RegisterFormComponent, LoginFormComponent],
@@ -15,6 +22,7 @@ import { LoginFormComponent } from './components/login-form/login-form.component
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     TuiRootModule,
     TuiDialogModule,
     TuiAlertModule,
@@ -23,6 +31,8 @@ import { LoginFormComponent } from './components/login-form/login-form.component
     TuiInputPasswordModule,
     TuiInputPhoneModule,
     TuiIslandModule,
+    TuiErrorModule,
+    TuiFieldErrorPipeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 interface RegisterFormData {
   firstName: string;
@@ -10,6 +10,32 @@ interface RegisterFormData {
   phoneNumber: string;
 }
 
+const emailValidator = (control: AbstractControl): ValidationErrors | null => {
+  if (typeof control.value !== 'string') {
+    return { valueIsNotString: 'Email is invalid' };
+  }
+
+  const email = control.value as string;
+  if (!email.includes('@')) {
+    return { missingAtChar: 'Email is invalid' };
+  }
+
+  return null;
+};
+
+const phoneValidator = (control: AbstractControl): ValidationErrors | null => {
+  if (typeof control.value !== 'string') {
+    return { valueIsNotString: 'Phone is invalid' };
+  }
+
+  const phone = control.value as string;
+  if (phone.length !== 14) {
+    return { incompletePhoneNumber: 'Phone number is incomplete' };
+  }
+
+  return null;
+};
+
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
@@ -19,7 +45,32 @@ export class RegisterFormComponent {
   @Input() title: string = '';
   @Output() submit = new EventEmitter<void>();
 
+  /*
+  registerForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    username: new FormControl(''),
+    password: new FormControl(''),
+    email: new FormControl(''),
+    phoneNumber: new FormControl(''),
+  });
+  */
+
+  registerForm = this.fb.group({
+    firstName: [''],
+    lastName: [''],
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    email: ['', [Validators.required, emailValidator]],
+    phoneNumber: ['', [phoneValidator]],
+  });
+
+  submitError: string | null = null;
+
+  constructor(private fb: FormBuilder) {}
+
   onSubmit() {
+    console.log('form: ', this.registerForm);
     this.submit.next();
   }
 }
