f38c7a3461d52f4d7478046dde8e76b6eaa6962e
Eduardo Macedo <eduzemacedo@hotmail.com>
2023-04-09 01:04:39 -0300

    More taiga ui stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
-import { TuiRootModule, TuiDialogModule, TuiAlertModule, TuiButtonModule } from '@taiga-ui/core';
-import { TuiInputModule, TuiIslandModule, TuiInputPasswordModule, TuiInputPhoneModule } from '@taiga-ui/kit';
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TuiButtonModule, TuiErrorModule } from '@taiga-ui/core';
+import {
+  TuiInputModule,
+  TuiIslandModule,
+  TuiInputPasswordModule,
+  TuiInputPhoneModule,
+  TuiFieldErrorPipeModule,
+} from '@taiga-ui/kit';
+import { TuiTableModule, TuiTablePaginationModule } from '@taiga-ui/addon-table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -21,6 +28,10 @@ import { AppComponent } from './app.component';
     TuiInputPasswordModule,
     TuiInputPhoneModule,
     TuiIslandModule,
+    TuiErrorModule,
+    TuiFieldErrorPipeModule,
+    TuiTableModule,
+    TuiTablePaginationModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
